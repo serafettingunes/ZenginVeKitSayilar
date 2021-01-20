2db7906e3853b297e3673ac7f6cf7fe2414214ad
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 17:57:20 +0300

    Add project files.

---
+++ ZenginVeKitSayi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ZenginVeKitSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tpl = 0;
+            for (int i = 21; i <= 1000; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        tpl += j;
+                    }
+
+                }
+                if (i > tpl)
+                {
+                    Console.WriteLine("{0} sayısı kıttır", i);
+                }
+                else if (i < tpl)
+                {
+                    Console.WriteLine("{0} sayısı zengindir", i);
+                }
+                tpl = 0;
+            }
+        }
+    }
+}
